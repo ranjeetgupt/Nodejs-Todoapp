7d97b1b4fd76d85df3015e1068b216eb9e9438b1
Ranjeet Gupta <ranjeetgupta6050@gmail.com>
2026-01-03 11:11:02 +0530

    Reset path

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(
   cors({
     
     origin: [
-      // process.env.FRONTEND_URL,
-    "https://nodejs-todoapp-lyja.onrender.com"],
+      process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
